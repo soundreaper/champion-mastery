f5fd403f2345635902c3171f26ac919b0e419a41
soundreaper@gmail.com <soundreaper@gmail.com>
2019-10-18 03:19:51 -0700

    complete API processing and returning to render template

--- app.py
+++ app.py
@@ -25,13 +25,14 @@ def identifyChampion(num):
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+# Recieves Summoner Username as a query and builds API URL and then sends back information recieved from Riot Games
 @app.route('/')
 def index():
     """Return homepage."""
     # Extract Summoner Name query from url
     summonerName = request.args.get('query')
     region = 'na'
-    #request.arge.get('region')
+    #request.args.get('region')
 
     #Checks to see if Summoner Name has a space in it and replaces with "%20" so API URL can be Built Properly
     if summonerName != "":
@@ -46,3 +47,41 @@ def index():
             "summonerName": summonerName,
             "APIKey": RG_API_KEY
         }
+
+        # Make an API call to Riot Games using the 'requests' library to parse information into JSON dictionaries
+        responseJSON_1 = requests.get("https://" + params.get("region") + "1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + params.get("summonerName") + "?api_key=" + params.get("APIKey")).json()
+        summonerID = str(responseJSON_1["id"])
+
+        responseJSON_2 = requests.get("https://" + params.get("region") + "1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + summonerID + "?api_key=" + params.get("APIKey")).json()
+        responseJSON_3 = requests.get("https://" + params.get("region") + "1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + summonerID + "?api_key=" + params.get("APIKey")).json()
+
+        if responseJSON_2[0]["queueType"] == "RANKED_SOLO_5x5":
+            infoNum = 0
+        else:
+            infoNum = 1
+
+        summoner_name = responseJSON_2[infoNum]["summonerName"]
+        level =  str(responseJSON_1['summonerLevel'])
+        queue_type = "Ranked Solo/Duo"
+
+        tier = responseJSON_2[infoNum]["tier"].lower().capitalize()
+        rank = responseJSON_2[infoNum]["rank"]
+        league_points = str(responseJSON_2[infoNum]["leaguePoints"])
+        
+        wins = str(responseJSON_2[infoNum]["wins"])
+        losses = str(responseJSON_2[infoNum]["losses"])
+
+        winrate_dec = responseJSON_2[infoNum]["wins"]/(responseJSON_2[infoNum]["wins"] + responseJSON_2[infoNum]["losses"])
+        winrate = str(round(winrate_dec * 100, 2))
+
+        champID = responseJSON_3[0]["championId"]
+        champion_name = identifyChampion(champID)
+        mastery_level = str(responseJSON_3[0]["championLevel"])
+        mastery_points = str(responseJSON_3[0]["championPoints"])
+        
+        # Render the 'index.html' template, passing all parsed parameters
+        return render_template("index.html", summoner_name=summoner_name, level=level, queue_type=queue_type,
+            tier=tier, rank=rank, league_points=league_points, wins=wins, losses=losses, winrate=winrate,
+            champion_name=champion_name, mastery_level=mastery_level, mastery_points=mastery_points)
+    else:
+        return render_template("index.html")
